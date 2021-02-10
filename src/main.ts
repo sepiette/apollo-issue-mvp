26c732af8bc50b8f269cfa86ac4c1285d7ec6ce0
Sara Piette <sepiette@gmail.com>
2021-02-10 10:32:03 -0500

    handle fake routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { AllExceptionsFilter } from './filters/all-exception.filter';
 import { AppModule } from './app.module';
+import { HttpStatus } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+    // ROBOTS.TXT & Favicon
+    app.use('/favicon.ico', function (req, res) {
+      res.sendStatus(HttpStatus.NO_CONTENT);
+    });
+  
+    app.use('/robots.txt', function (req, res) {
+      res.type('text/plain');
+      res.send('User-agent: *\nDisallow: /');
+    });
+    
 	app.useGlobalFilters(new AllExceptionsFilter());
 
 	await app.listen(4000);
